6524b716e422f29c1fef4a23e03a794959a7d877
Phil Weir <phil.weir@flaxandteal.co.uk>
2019-10-02 12:29:04 +0100

    make sure module is correctly spotted

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.7'
     ],
     keywords='tmux learning',
     install_requires=[
